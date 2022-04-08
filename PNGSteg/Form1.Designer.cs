ff0c2d4c6b68bcaeef37a5a521ac2d420a1f137d
Oktikk <oktikk@gmail.com>
2022-04-08 20:19:30 +0200

    Add project files.

---
+++ PNGSteg/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace PNGSteg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonHide = new System.Windows.Forms.Button();
+            this.buttonShow = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(167, 325);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 0;
+            this.loadButton.Text = "Load image";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(392, 255);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 378);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(393, 91);
+            this.textBox1.TabIndex = 2;
+            // 
+            // buttonHide
+            // 
+            this.buttonHide.Location = new System.Drawing.Point(124, 492);
+            this.buttonHide.Name = "buttonHide";
+            this.buttonHide.Size = new System.Drawing.Size(75, 23);
+            this.buttonHide.TabIndex = 3;
+            this.buttonHide.Text = "Hide";
+            this.buttonHide.UseVisualStyleBackColor = true;
+            this.buttonHide.Click += new System.EventHandler(this.buttonHide_Click);
+            // 
+            // buttonShow
+            // 
+            this.buttonShow.Location = new System.Drawing.Point(205, 492);
+            this.buttonShow.Name = "buttonShow";
+            this.buttonShow.Size = new System.Drawing.Size(75, 23);
+            this.buttonShow.TabIndex = 4;
+            this.buttonShow.Text = "Show";
+            this.buttonShow.UseVisualStyleBackColor = true;
+            this.buttonShow.Click += new System.EventHandler(this.buttonShow_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(177, 356);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Input text";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 534);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonShow);
+            this.Controls.Add(this.buttonHide);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.loadButton);
+            this.Name = "Form1";
+            this.Text = "Hide text in PNG";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonHide;
+        private System.Windows.Forms.Button buttonShow;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PNGSteg/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace PNGSteg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap bmp;
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        bool imgLoaded = false;
+        public void LoadImg()
+        {
+            openFileDialog1.FileName = "turkishEmbassy.png";
+            openFileDialog1.Filter = "Image files (*.png)|*.png";
+            openFileDialog1.InitialDirectory = desktopPath;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog1.FileName;
+                pictureBox1.ImageLocation = filePath;
+                CreateBmpFromPng(filePath);
+            }
+            
+        }
+
+        public void CreateBmpFromPng(string filePath)
+        {
+            bmp = new Bitmap(filePath);
+            imgLoaded = true;
+        }
+
+        public void ClearLSB()
+        {
+            Color color;
+            int r,g,b;
+
+            for(int i = 0; i < bmp.Height; i++)
+            {
+                for(int j = 0; j < bmp.Width; j++)
+                {
+                    color = bmp.GetPixel(j, i);
+                    r = color.R - color.R % 2;
+                    g = color.G - color.G % 2;
+                    b = color.B - color.B % 2;
+                    bmp.SetPixel(j,i,Color.FromArgb(r,g,b));
+                }
+            }
+        }
+
+        public void HideTextInImage(string text)
+        {
+            Color color;
+            int r, g, b;
+            int bitPointer = 0;
+            int bytePointer = 0;
+            string charBits = GetCharBits(text[bytePointer]);
+
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    color = bmp.GetPixel(j, i);
+                    r = color.R;
+                    g = color.G;
+                    b = color.B;
+
+                    for (int k = 0; k < 3; k++)
+                    {
+                        if(bitPointer > charBits.Length-1)
+                        {
+                            bytePointer++;
+                            if(bytePointer > text.Length-1)
+                            {
+                                return;
+                            }
+                            bitPointer = 0;
+                            charBits = GetCharBits(text[bytePointer]);
+                        }
+                        switch (k)
+                        {
+                            case 0:
+                                r = color.R + Convert.ToInt32(charBits[bitPointer].ToString());
+                                break;
+                            case 1:
+                                g = color.G + Convert.ToInt32(charBits[bitPointer].ToString());
+                                break;
+                            case 2:
+                                b = color.B + Convert.ToInt32(charBits[bitPointer].ToString());
+                                break;
+                        }
+                        bmp.SetPixel(j, i, Color.FromArgb(r, g, b));
+                        bitPointer++;
+                    }
+
+                }
+            }
+        }
+
+        public void GetTextFromImage()
+        {
+            Color color;
+            string text = "";
+            string charBits = "";
+            int bitCounter = 0;
+
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    color = bmp.GetPixel(j, i);
+
+                    for (int k = 0; k < 3; k++)
+                    {
+                        if (bitCounter > 8)
+                        {
+                            if (charBits == "000000000")
+                            {
+                                saveFileDialog1.FileName = "text.txt";
+                                saveFileDialog1.Filter = "Text file (*.txt)|*.txt";
+                                saveFileDialog1.InitialDirectory = desktopPath;
+                                if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+                                {
+                                    string filePath = saveFileDialog1.FileName;
+                                    File.WriteAllText(filePath, text);
+                                }
+                                return;
+                            }
+                            bitCounter = 0;
+                            text += Convert.ToChar(Convert.ToInt32(charBits, 2));
+                            charBits = "";
+                        }
+                        switch (k)
+                        {
+                            case 0:
+                                charBits += color.R % 2;
+                                break;
+                            case 1:
+                                charBits += color.G % 2;
+                                break;
+                            case 2:
+                                charBits += color.B % 2;
+                                break;
+                        }
+                        bitCounter++;
+                    }
+
+                }
+            }
+        }
+
+        public string GetCharBits(Char c)
+        {
+            return Convert.ToString(c, 2).PadLeft(9, '0');
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            LoadImg();
+        }
+
+        private void buttonHide_Click(object sender, EventArgs e)
+        {
+            if (!imgLoaded)
+            {
+                MessageBox.Show("Image not loaded","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }
+            ClearLSB();
+            HideTextInImage(textBox1.Text);
+            saveFileDialog1.FileName = "picture.png";
+            saveFileDialog1.Filter = "Image file (*.png)|*.png";
+            saveFileDialog1.InitialDirectory = desktopPath;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog1.FileName;
+                bmp.Save(filePath);
+            }
+        }
+
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+            if (!imgLoaded)
+            {
+                MessageBox.Show("Image not loaded", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            GetTextFromImage();
+        }
+    }
+}
